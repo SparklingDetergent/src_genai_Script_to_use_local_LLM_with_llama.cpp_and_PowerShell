dd65ad00e7f41df2c2005fe0087ec36e22417601
SparklingDetergent <83e726g330vth03@gmail.com>
2024-08-19 03:24:13 +0900

    multi prompt enabled

--- llama-server.bat
+++ llama-server.bat
@@ -1,8 +1,8 @@
 REM ƒTƒ“ƒvƒ‹
 
-cd /d C:\work\models\llama-b3058-bin-win-openblas-x64
+cd /d C:\work\models\llama-b3496-bin-win-openblas-x64
 
-server.exe -m Phi-3-mini-4k-instruct-q4.gguf -ngl 0 -c 4096 -n -1 -t 2 --host 127.0.0.1 --port 8085
+llama-server.exe -m gemma-2-2b-it-Q4_K_M.gguf -ngl 0 -c 8192 -n -1 -t 2 --host 127.0.0.1 --port 8087
 
 pause
 pause
