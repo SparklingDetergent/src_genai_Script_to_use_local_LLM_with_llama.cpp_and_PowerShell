e2bbd7d5e235b4066fb65e259a9ed0e7b1b2581b
SparklingDetergent <83e726g330vth03@gmail.com>
2024-08-24 09:10:57 +0900

    multi prompt  config change

---
+++ gemma2_multi_conf_sample/ChatForWindowsFunction _multi_sample.bat
@@ -0,0 +1,42 @@
+cd /d C:\work\powershell
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\メタ認知\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\セマンティック検索\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\水平思考\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\人間のように\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\徐々に複雑\ChatForWindowsFunction.bat"
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\推論プロセス\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\要約\ChatForWindowsFunction.bat"
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\ステップバイステップ\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\AtoZ\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\ゴールシーク\ChatForWindowsFunction.bat"
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\ハルシネーション\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\間違い網羅\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\分からないとき\ChatForWindowsFunction.bat"
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\やればできる\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\コンペ\ChatForWindowsFunction.bat"
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\泥酔\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\脱獄\ChatForWindowsFunction.bat"
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\関連性抽出\ChatForWindowsFunction.bat"
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\考える時間\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\ステップバイステップ表\ChatForWindowsFunction.bat"
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\レベル分け\ChatForWindowsFunction.bat"
+
+
+
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\聞き返し\ChatForWindowsFunction.bat"
+ping -n 2 127.0.0.1  & start cmd /c ".\multi_sample\質問箇条書き\ChatForWindowsFunction.bat"
--- phi-3.1_conf_sample/llama-server.bat
+++ phi-3.1_conf_sample/llama-server.bat
@@ -2,7 +2,7 @@ REM ƒTƒ“ƒvƒ‹
 
 cd /d C:\work\models\llama-b3496-bin-win-openblas-x64
 
-llama-server.exe -m Phi-3.1-mini-4k-instruct-Q4_K_M.gguf -ngl 0 -c 4096 -n -1 -t 2 --host 127.0.0.1 --port 8086
+llama-server.exe -m Phi-3.1-mini-4k-instruct-Q4_K_M.gguf -np 32 -ngl 0 -b 1024 -ub 64 -c 4096 -ns 4096 -n -1 -cb -dt 0.1 -t 2  --host 127.0.0.1 --port 8086
 
 pause
 pause
