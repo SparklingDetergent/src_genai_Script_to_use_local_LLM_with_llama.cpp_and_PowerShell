b51a1e16b5e7d4b459e2450119a4056eede96432
SparklingDetergent <83e726g330vth03@gmail.com>
2024-10-12 20:47:09 +0900

    stream enabled

--- phi-3.1_conf_sample/ChatForWindowsFunction.bat
+++ phi-3.1_conf_sample/ChatForWindowsFunction.bat
@@ -1,6 +1,5 @@
 REM ƒTƒ“ƒvƒ‹
 
-cd /d C:\work\powershell\Phi-3.1\
 
 powershell -NoProfile -ExecutionPolicy Bypass  -Command "$VerbosePreference='Continue';$ErrorActionPreference='Stop';" ./ChatForWindows.ps1 -configPath './configure.json' -systemPromptPath './systemPrompt.conf' -assistantPromptPath './assistantPrompt.conf' -stopWordPath './stopWord.conf' -outputPath './output'
 
