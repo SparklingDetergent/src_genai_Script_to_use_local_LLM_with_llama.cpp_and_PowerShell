2a93405323cf9e4307a8c97209cfc564f777377e
SparklingDetergent <83e726g330vth03@gmail.com>
2024-10-12 20:20:03 +0900

    stream enabled

--- llama-server.bat
+++ llama-server.bat
@@ -1,8 +1,8 @@
 REM ƒTƒ“ƒvƒ‹
 
-cd /d C:\work\models\llama-b3496-bin-win-openblas-x64
+cd /d C:\work\models\llama-b3808-bin-win-openblas-x64
 
-llama-server.exe -m gemma-2-2b-it-Q4_K_M.gguf -np 32 -ngl 0 -b 1024 -ub 64 -c 8192 -ns 8192 -n -1 -cb -dt 0.1 -t 2  --host 127.0.0.1 --port 8087
+llama-server.exe -m qwen2.5-coder-7b-instruct-q5_0.gguf -np 4 -ngl 0 -b 1024 -ub 64 -c 8192 -n -1 -cb -dt 0.1 -t 2  --host 127.0.0.1 --port 8088
 
 pause
 pause
--- llama-server.sh
+++ llama-server.sh
@@ -1,1 +1,3 @@
-./llama-server -m ./EZO-Common-T2-2B-gemma-2-it.Q4_K_M.iMatrix.gguf -np 128 -ngl 99 -b 1024 -ub 64 -c 8192 -ns 8192 -n -1 -cb -dt 0.1 -t 4  --host 127.0.0.1 --port 8087
+cd /Users/user/Public/work/models/llama-b3808-bin-macos-arm64/build/bin
+
+./llama-server -m ./qwen2.5-coder-7b-instruct-q5_0.gguf -np 4 -ngl 33 -b 1024 -ub 64 -c 8192 -n -1 -cb -dt 0.1 -t 4 -fa --host 192.168.10.10 --port 8088
