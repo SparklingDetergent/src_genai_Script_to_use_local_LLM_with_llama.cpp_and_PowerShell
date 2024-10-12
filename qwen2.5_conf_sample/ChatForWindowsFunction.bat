0da5071e9ba0015bd90b484eda2e8e3fb849f8b6
SparklingDetergent <83e726g330vth03@gmail.com>
2024-10-12 20:49:55 +0900

    stream enabled

--- qwen2.5_conf_sample/ChatForWindowsFunction.bat
+++ qwen2.5_conf_sample/ChatForWindowsFunction.bat
@@ -1,6 +1,5 @@
 REM ƒTƒ“ƒvƒ‹
 
-cd /d C:\work\powershell\Qwen2\
 
 powershell -NoProfile -ExecutionPolicy Bypass  -Command "$VerbosePreference='Continue';$ErrorActionPreference='Stop';" ./ChatForWindows.ps1 -configPath './configure.json' -systemPromptPath './systemPrompt.conf' -assistantPromptPath './assistantPrompt.conf' -stopWordPath './stopWord.conf' -outputPath './output'
 
