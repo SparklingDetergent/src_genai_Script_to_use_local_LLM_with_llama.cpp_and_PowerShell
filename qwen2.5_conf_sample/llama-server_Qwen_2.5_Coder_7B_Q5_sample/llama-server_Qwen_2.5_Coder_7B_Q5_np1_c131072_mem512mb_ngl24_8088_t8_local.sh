45657474815f85cde8302cc2d901199c523fe5be
SparklingDetergent <83e726g330vth03@gmail.com>
2024-10-12 20:56:09 +0900

    llama-server_Qwen_2.5_Coder_7B_Q5_sample

---
+++ qwen2.5_conf_sample/llama-server_Qwen_2.5_Coder_7B_Q5_sample/llama-server_Qwen_2.5_Coder_7B_Q5_np1_c131072_mem512mb_ngl24_8088_t8_local.sh
@@ -0,0 +1,1 @@
+./llama-server -m ./qwen2.5-coder-7b-instruct-q5_0.gguf -np 1 -ngl 24 -b 1024 -ub 64 -c 131072 -n -1 -cb -dt 0.1 -t 8 -fa --host 127.0.0.1 --port 8088
---
+++ qwen2.5_conf_sample/llama-server_Qwen_2.5_Coder_7B_Q5_sample/llama-server_Qwen_2.5_Coder_7B_Q5_np1_c8192_mem512mb_ngl4_8091_t2.sh
@@ -0,0 +1,1 @@
+./llama-server -m ./qwen2.5-coder-7b-instruct-q5_0.gguf -np 1 -ngl 4 -b 1024 -ub 64 -c 8192 -n -1 -cb -dt 0.1 -t 2 -fa --host 192.168.10.10 --port 8091
